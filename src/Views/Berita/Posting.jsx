763bcb50f1eb15f9ba25de595a0e594c56bac5d9
Genta-arya <mgentaarya@gmail.com>
2025-03-22 07:51:19 +0700

    first commitss

--- src/Views/Berita/Posting.jsx
+++ src/Views/Berita/Posting.jsx
@@ -2,16 +2,21 @@ import React, { useState } from 'react';
 import FroalaEditor from 'react-froala-wysiwyg';
 import 'froala-editor/js/plugins.pkgd.min.js';
 import 'froala-editor/css/froala_editor.pkgd.min.css';
+import DOMPurify from 'dompurify';
 
 const Posting = () => {
   const [editorContent, setEditorContent] = useState('');
+  const [previewContent, setPreviewContent] = useState('');
 
   const handleModelChange = (content) => {
     setEditorContent(content);
   };
 
   const handlePostClick = () => {
-    console.log('Posting content:', editorContent);
+    // Sanitasi konten sebelum menampilkan di bawah
+    const sanitizedContent = DOMPurify.sanitize(editorContent);
+    setPreviewContent(sanitizedContent); // Menyimpan hasil sanitasi untuk preview
+    console.log('Posting content:', sanitizedContent);
   };
 
   return (
@@ -22,6 +27,14 @@ const Posting = () => {
         onModelChange={handleModelChange}
       />
       <button onClick={handlePostClick}>Posting</button>
+      
+      {/* Preview content */}
+      {previewContent && (
+        <div>
+          <h3>Preview Blog:</h3>
+          <div dangerouslySetInnerHTML={{ __html: previewContent }} />
+        </div>
+      )}
     </div>
   );
 };
